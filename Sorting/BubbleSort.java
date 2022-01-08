753f8b5aa59ba0f6d68918043a3857f0cc645808
Dheeruyendra Pratap Singh <pratapsingh31dec2002@gmail.com>
2022-01-08 18:08:24 +0530

    optimized bubble sort

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -6,14 +6,20 @@ public class BubbleSort {
           int[] arr = {45,98,100,120,500,1000,0,-100,-200,-10};
 
           for(int i=0; i<arr.length-1; i++){
-
+              boolean isSwapped = false;
            for(int j=0; j<arr.length-i-1; j++){
                 if(arr[j]  > arr[j+1]){
+                    isSwapped = true;
+
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
            }
+
+             if(!isSwapped){
+                 break;
+             }
 
           }
          for(int i=0; i<arr.length; i++){
