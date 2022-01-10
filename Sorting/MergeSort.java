41fb9712dac94cff5a9b5b614c78ffa81198fb22
Dheeruyendra <pratapsingh31dec2002@gmail.com>
2022-01-10 14:00:28 +0530

    added comments in merge sort

--- Sorting/MergeSort.java
+++ Sorting/MergeSort.java
@@ -6,25 +6,31 @@ public class MergeSort{
   
 
    static void merge(int[] arr, int left, int mid, int right){
+         
+    //size of first subarray
            int size1 = mid-left+1;
+    //size of second subarray       
            int size2 = right-mid;
-
+     
+           //first subarray of size1
            int[] temp1 = new int[size1];
+           //second subarray of size2
            int[] temp2 = new int[size2];
            
-         
-
+           //copying element in first subarray (elements are being copied from left of parent array till mid)
            for(int i=0; i<size1; i++){
                temp1[i] = arr[i+left];           
             }
          
+           //copying element in second subarray (elements are being copied from mid+1 of parent array till right)
             for(int i=0; i<size2; i++){
                 temp2[i] = arr[i+mid+1];
             }
 
           int  t1=0;
-         int   t2=0;
-            int p=left;
+          int  t2=0;
+          // 'p' pointer for filling elements in sorted order (parent array starting index is left) 
+          int p=left;
 
             while(t1<size1 && t2<size2){
                 if(temp1[t1] <= temp2[t2]){
@@ -49,12 +55,16 @@ public class MergeSort{
 
      static void mergeSort(int[] arr, int si, int ei){
            if(si<ei){
-
+           //mid point 
             int mid = si+(ei-si)/2;
-
+              
+            //mergeSort is called on first half of array
                mergeSort(arr, si, mid);
+            
+            //mergeSort is called on second half of array   
                mergeSort(arr, mid+1, ei);
-
+            
+            //mergeing both sorted subarray   
                merge(arr, si, mid, ei);
            }
      }
